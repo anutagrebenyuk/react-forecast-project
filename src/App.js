09d5aa3627e509d3fd6cc4b178d1ace3afb05824
anutagrebenyuk <107403620+anutagrebenyuk@users.noreply.github.com>
2022-08-30 18:42:18 +0300

    npm bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <button className="btn btn-primary mt-5">HH</button>
       <p>
         {" "}
         This site is coded by Hanna Hrebeniuk and is{" "}
