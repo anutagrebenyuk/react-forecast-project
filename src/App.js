5b908a288003b8cb349e03542f9cec006e8d8e5b
anutagrebenyuk <107403620+anutagrebenyuk@users.noreply.github.com>
2022-08-30 18:05:53 +0300

    added link to GitHub repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <p>
+        {" "}
+        This site is coded by Hanna Hrebeniuk and is{" "}
+        <a href="https://github.com/anutagrebenyuk/react-forecast-project">
+          open-sourced
+        </a>{" "}
+        on GitHub
+      </p>
     </div>
   );
 }
